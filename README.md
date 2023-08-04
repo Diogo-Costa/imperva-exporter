d5947237154b44749b9249d5481b95993696c9d7
Diogo Costa <9933288+Diogo-Costa@users.noreply.github.com>
2023-08-03 23:19:44 -0300

    Add new metrics threats and visits_dist_summary, add new args granularity #minor

--- README.md
+++ README.md
@@ -33,6 +33,8 @@ Application Options:
         ApiId to connect imperva api metrics.  (required) 
   --interval int
         sleep to run get metrics again. (default -> 300)
+  --granularity int
+        time interval in milliseconds between data points for time series statistics. (default -> 600)
   --port int
         Exporter Port expose. (default -> 8956)
 ```
@@ -58,22 +60,36 @@ docker run --rm -it -p 8956:8956 ghcr.io/diogo-costa/imperva-exporter:latest /ap
 # HELP incap_visits_humans_timeseries Number of visits by type (Humans) over time.
 # TYPE incap_visits_humans_timeseries gauge
 incap_visits_humans_timeseries{api_id="api.stats.visits_timeseries.human",name="Human visits",site_name="xxx.xxx.xxx"} xx
+...
 
 # HELP incap_visits_bots_timeseries Number of visits by type (Bots) over time.
 # TYPE incap_visits_bots_timeseries gauge
 incap_visits_bots_timeseries{api_id="api.stats.visits_timeseries.bot",name="Bot visits",site_name="xxx.xxx.xxx"} xx
+...
 
 # HELP incap_caching_hits_standard_timeseries Number of requests and bytes that were cached by the Imperva network, with one day resolution, with info regarding the caching mode (standard).
 # TYPE incap_caching_hits_standard_timeseries gauge
 incap_caching_hits_standard_timeseries{api_id="api.stats.caching_timeseries.hits.standard",name="Standard Requests Caching",site_name="xxx.xxx.xxx"} xx
+...
 
 # HELP incap_caching_hits_advanced_timeseries Number of requests and bytes that were cached by the Imperva network, with one day resolution, with info regarding the caching mode (advanced).
 # TYPE incap_caching_hits_advanced_timeseries gauge
 incap_caching_hits_advanced_timeseries{api_id="api.stats.caching_timeseries.bytes.standard",name="Standard Bandwidth Caching",site_name="xxx.xxx.xxx"} xx
+...
 
 # HELP incap_rules_timeseries List of security rules with a series of reported incidents for each rule with the specified granularity.
 # TYPE incap_rules_timeseries gauge
 incap_rules_timeseries{action="Alert",name="rateLimitSiteGeneral",site_name="xxx.xxx.xxx"} xx
+...
+
+# HELP incap_threats_incidents Total number of threats by type with additional information regarding the security rules configuration.
+# TYPE incap_threats_incidents gauge
+incap_threats_incidents{name="Visitors from denylisted IPs",rule_id="api.acl.blacklisted_ips",site_name="xxx.xx.xxx.xxxx"} xx
+...
+
+# HELP incap_visits_summary Total number of visits per client application and country.
+# TYPE incap_visits_summary gauge
+incap_visits_summary{app_type="null",country="pe",id="api.stats.visits_dist_summary.country",name="Visits by country",site_name="xxx.xx.xxxx.xxxx"} xx
 ...
 ```
 
--- exporter/imperva_exporter.py
+++ exporter/imperva_exporter.py
@@ -4,19 +4,21 @@ from datetime import date, datetime, timedelta
 import requests
 import time
 import json
+import sys
 import argparse
 
 parser = argparse.ArgumentParser(description='Prometheus exporter for `Imperva WAF` metrics')
 parser.add_argument("--imperva_endpoint", help="Imperva WAF Api Endpoint", default="https://my.imperva.com")
 parser.add_argument("--account_id", help="Imperva WAF Account ID")
 parser.add_argument('--interval', type=int, metavar='N', default=300, help='check last N seconds, default: 300')
+parser.add_argument('--granularity', type=int, metavar='N', default=600, help='check last N seconds, default: 600')
 parser.add_argument("--api_key", help="ApiKey to connect imperva api metrics", default="")
 parser.add_argument("--api_id", help="ApiId to connect imperva api metrics")
 parser.add_argument("--port", type=int, help="Exporter Port expose", default=8956)
 args = parser.parse_args()
 
 ENDPOINT = args.imperva_endpoint
-ACCOUNT_ID = args.account_id 
+ACCOUNT_ID = args.account_id
 HEADERS = {
     'Accept': "application/json",
     'x-API-Key': args.api_key,
@@ -34,6 +36,10 @@ prom = {
         ['site_name', 'api_id', 'name']),
     'incap_rules_timeseries': Gauge('incap_rules_timeseries', 'List of security rules with a series of reported incidents for each rule with the specified granularity.',
         ['site_name', 'name', 'action']),
+    'incap_threats_incidents': Gauge('incap_threats_incidents', 'Total number of threats by type with additional information regarding the security rules configuration.',
+        ['site_name', 'name', 'rule_id']),
+    'incap_visits_summary': Gauge('incap_visits_summary', 'Total number of visits per client application and country.',
+        ['site_name', 'name', 'id', "country", "app_type"]),
 }
 
 
@@ -45,8 +51,8 @@ def get_imperva_metrics(site_id):
                 'account_id': ACCOUNT_ID,
                 'time_range': 'today',
                 'site_id': site_id,
-                'stats': 'visits_timeseries,caching_timeseries,incap_rules_timeseries',
-                'granularity': '1800000'
+                'stats': 'visits_timeseries,caching_timeseries,incap_rules_timeseries,threats,visits_dist_summary',
+                'granularity': int(args.granularity * 1000)
             }, headers=HEADERS)
             if not response.ok:
                 response.raise_for_status()
@@ -83,15 +89,60 @@ if __name__ == "__main__":
                 print(metrics_dict)
                 ## Generate prometheus metrics for visits_timeseries
                 if metrics_dict["visits_timeseries"][0]["data"] != []:
-                    prom['incap_visits_humans_timeseries'].labels(site_name=site["domain"],api_id=metrics_dict["visits_timeseries"][0]["id"],name=metrics_dict["visits_timeseries"][0]["name"]).set(metrics_dict["visits_timeseries"][0]["data"][-1][-1])
+                    prom['incap_visits_humans_timeseries'].labels(
+                        site_name=site["domain"],
+                        api_id=metrics_dict["visits_timeseries"][0]["id"],
+                        name=metrics_dict["visits_timeseries"][0]["name"]
+                    ).set(metrics_dict["visits_timeseries"][0]["data"][-1][-1])
                 if metrics_dict["visits_timeseries"][1]["data"] != []:
-                    prom['incap_visits_bots_timeseries'].labels(site_name=site["domain"],api_id=metrics_dict["visits_timeseries"][1]["id"],name=metrics_dict["visits_timeseries"][1]["name"]).set(metrics_dict["visits_timeseries"][1]["data"][-1][-1])
+                    prom['incap_visits_bots_timeseries'].labels(
+                        site_name=site["domain"],
+                        api_id=metrics_dict["visits_timeseries"][1]["id"],
+                        name=metrics_dict["visits_timeseries"][1]["name"]
+                    ).set(metrics_dict["visits_timeseries"][1]["data"][-1][-1])
                 if metrics_dict["caching_timeseries"][0]["data"] != []:
-                    prom['incap_caching_hits_standard_timeseries'].labels(site_name=site["domain"],api_id=metrics_dict["caching_timeseries"][0]["id"],name=metrics_dict["caching_timeseries"][0]["name"]).set(metrics_dict["caching_timeseries"][0]["data"][-1][-1])
+                    prom['incap_caching_hits_standard_timeseries'].labels(
+                        site_name=site["domain"],
+                        api_id=metrics_dict["caching_timeseries"][0]["id"],
+                        name=metrics_dict["caching_timeseries"][0]["name"]
+                    ).set(metrics_dict["caching_timeseries"][0]["data"][-1][-1])
                 if metrics_dict["caching_timeseries"][1]["data"] != []:
-                    prom['incap_caching_hits_advanced_timeseries'].labels(site_name=site["domain"],api_id=metrics_dict["caching_timeseries"][1]["id"],name=metrics_dict["caching_timeseries"][1]["name"]).set(metrics_dict["caching_timeseries"][1]["data"][-1][-1])
+                    prom['incap_caching_hits_advanced_timeseries'].labels(
+                        site_name=site["domain"],
+                        api_id=metrics_dict["caching_timeseries"][1]["id"],
+                        name=metrics_dict["caching_timeseries"][1]["name"]
+                    ).set(metrics_dict["caching_timeseries"][1]["data"][-1][-1])
                 if metrics_dict["incap_rules_timeseries"] != []:
-                    prom['incap_rules_timeseries'].labels(site_name=site["domain"],action=metrics_dict["incap_rules_timeseries"][0]["action"],name=metrics_dict["incap_rules_timeseries"][0]["name"]).set(metrics_dict["incap_rules_timeseries"][0]["incidents"][-1][-1])
+                    prom['incap_rules_timeseries'].labels(
+                        site_name=site["domain"],
+                        action=metrics_dict["incap_rules_timeseries"][0]["action"],
+                        name=metrics_dict["incap_rules_timeseries"][0]["name"]
+                    ).set(metrics_dict["incap_rules_timeseries"][0]["incidents"][-1][-1])
+                if metrics_dict["threats"] != []:
+                    for threats_incidents in metrics_dict["threats"]:
+                        prom['incap_threats_incidents'].labels(
+                            site_name=site["domain"],
+                            rule_id=threats_incidents["id"],
+                            name=threats_incidents["name"]
+                        ).set(threats_incidents["incidents"])
+                if metrics_dict["visits_dist_summary"][0]["data"] != []:
+                    for country in metrics_dict["visits_dist_summary"][0]["data"]:
+                        prom['incap_visits_summary'].labels(
+                            site_name=site["domain"],
+                            id=metrics_dict["visits_dist_summary"][0]["id"],
+                            country=country[0],
+                            app_type="null",
+                            name=metrics_dict["visits_dist_summary"][0]["name"]
+                        ).set(country[1])
+                if metrics_dict["visits_dist_summary"][1]["data"] != []:
+                    for country in metrics_dict["visits_dist_summary"][1]["data"]:
+                        prom['incap_visits_summary'].labels(
+                            site_name=site["domain"],
+                            id=metrics_dict["visits_dist_summary"][1]["id"],
+                            country="null",
+                            app_type=country[0],
+                            name=metrics_dict["visits_dist_summary"][1]["name"]
+                        ).set(country[1])
 
             time.sleep(args.interval)
     except Exception as e:
